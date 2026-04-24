43fe9b424468883c4d6285e5cd73511709fc957a
AnuragBojja <anuragbojja19@gmail.com>
2026-04-23 21:22:54 -0500

    80 service

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "main" {
   for_each = var.service_name
   source = "git::https://github.com/AnuragBojja/terraform-roboshop-dev-service-config.git?ref=main"
+  env = var.env
   service_name = each.key
   priority = each.value.rule_priority
+  domain_name = var.domain_name
 }
