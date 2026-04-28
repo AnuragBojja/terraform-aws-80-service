d8810e3ea10878387c85ccd84c13cb6a45082a6a
AnuragBojja <anuragbojja19@gmail.com>
2026-04-28 18:07:49 -0500

    '80-service'

--- variables.tf
+++ variables.tf
@@ -12,24 +12,24 @@ variable "domain_name" {
 }
 variable "service_name" {
   default = {
-    catalogue = {
-      rule_priority = 10
-    }
-    user = {
-      rule_priority = 20
-    }
-    cart = {
-      rule_priority = 30
-    }
+    # catalogue = {
+    #   rule_priority = 10
+    # }
+    # user = {
+    #   rule_priority = 20
+    # }
+    # cart = {
+    #   rule_priority = 30
+    # }
     payment = {
       rule_priority = 40
     }
-    shipping = {
-      rule_priority = 50
-    }
-    frontend = {
-      rule_priority = 10
-    }
+    # shipping = {
+    #   rule_priority = 50
+    # }
+    # frontend = {
+    #   rule_priority = 10
+    # }
   }
 }
 
